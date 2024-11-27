c0d62a57f2f83934b0dbba2d2d2deca192cd34d5
Jane <lopezjane18021@gmail.com>
2024-11-27 15:03:21 +0800

    ARO

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request, redirect, session, flash, jsonify
 from dbhelper import *
+from flask_session import Session
 
 app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+
+Session(app)
 
 uploadfolder = 'static/img'
 app.secret_key = '!@#$%^'
